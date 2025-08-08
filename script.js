e7a5fb834543bd7387d9c80ef18bc4a568b5a735
MDeak97 <michaeldeyak90@gmail.com>
2025-08-08 21:56:22 +1000

    Answering Q1.1

---
+++ script.js
@@ -0,0 +1,9 @@
+// File: script.js
+// Description: A file to answer questions from JSAT 2 Part 1
+// Author: Michael Deak
+// Date: 08/08/2025
+// Version: 1.0.0
+
+// Defining the array
+const myArray = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
+console.log(myArray);
