b27fa1de467eec740cf8d96142a9daf096c7f176
MDeak97 <michaeldeyak90@gmail.com>
2025-08-08 22:14:48 +1000

    Answering Q1.2

--- script.js
+++ script.js
@@ -7,3 +7,9 @@
 // Defining the array
 const myArray = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
 console.log(myArray);
+
+// Sorting the array in ascending order
+const sortedArray = myArray.sort(function (a, b) {
+  return a - b;
+});
+console.log(sortedArray);
