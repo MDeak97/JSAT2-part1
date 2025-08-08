d0983917d257d87799b0ff5a56d073c939d59de7
MDeak97 <michaeldeyak90@gmail.com>
2025-08-08 22:56:06 +1000

    Updating Q1.3 and answering Q1.4

--- script.js
+++ script.js
@@ -4,19 +4,24 @@
 // Date: 08/08/2025
 // Version: 1.0.0
 
-// Defining the array
+// Q1.1 - Defining the array.
 const myArray = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
 console.log(myArray);
 
-// Sorting the array in ascending order
+// Q1.2 - Sorting the array in ascending order.
 const sortedArray = myArray.sort(function (a, b) {
   return a - b;
 });
 console.log(sortedArray);
 
-// Adding three numbers to the sorted array and keeping it in ascending order.
+// Q1.3 - Adding three numbers to the sorted array and keeping it in ascending order.
 sortedArray.splice(6, 0, 19, 23, 30);
-const sortedArrayA = sortedArray.sort(function (a, b) {
+const sortedArrayB = sortedArray.sort(function (a, b) {
   return a - b;
 });
-console.log(sortedArrayA);
+console.log(sortedArrayB);
+
+// Q1.4 - Removing 8, 31 from the array and keeping the array in ascending order.
+sortedArrayB.splice(2, 1);
+sortedArrayB.splice(10, 1);
+console.log(sortedArrayB);
