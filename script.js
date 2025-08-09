a87c62c15a32cb141abb5bc860960b99494774d1
MDeak97 <michaeldeyak90@gmail.com>
2025-08-09 15:40:49 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // File: script.js
 // Description: A file to answer questions from JSAT 2 Part 1
 // Author: Michael Deak
-// Date: 08/08/2025
+// Date: 09/08/2025
 // Version: 1.0.0
 
 // Q1.1 - Defining the array.
@@ -21,7 +21,44 @@ const sortedArrayB = sortedArray.sort(function (a, b) {
 });
 console.log(sortedArrayB);
 
-// Q1.4 - Removing 8, 31 from the array and keeping the array in ascending order.
+// Q1.4 - Removing 8 and 31 from the array and keeping the array in ascending order.
 sortedArrayB.splice(2, 1);
 sortedArrayB.splice(10, 1);
 console.log(sortedArrayB);
+
+// Q1.5 - Creating a sequential search function to find a specific value within an array.
+// Array we are searching
+const searchArray = [20, 12, 55, 70, 100, 30, 28, 19, 18, 92, 66];
+console.log(searchArray);
+
+// Value we are searching for within array
+let value = 1;
+console.log(`Value: ${value}`);
+
+// Creating Sequential search function
+function sequentialSearch(arraySearch, target) {
+  // Setting found variable to -1
+  let found = -1;
+  // Logging the array to search and the value to find
+  console.log(`Array to search is: ${arraySearch}`);
+  console.log(`Value to search is: ${value}`);
+  // Loop through array
+  for (i = 0; i < arraySearch.length; i++) {
+    // Log out element
+    console.log(`ArraySearch[${i}]: ${arraySearch[i]}`);
+    // Test to see if we found target
+    if (arraySearch[i] == target) {
+      found = i;
+      break;
+    }
+  }
+  return found;
+}
+// Call function sequentialSearch
+const result = sequentialSearch(searchArray, value);
+// Output results
+if (result == -1) {
+  console.log(`The value of ${value} was not found`);
+} else {
+  console.log(`The value of ${value} was found at index ${result}`);
+}
